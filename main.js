5b7ba6fbdec85afb08f09fe50e70c2e5aa3e501a
Gustavo Lemos <81194465+GustaJLemos@users.noreply.github.com>
2021-12-02 12:01:22 -0300

    Atualizando o código com responsividade para desktop e mobile

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+    github: 'GustaJLemos',
+    youtube: 'Rocketseat',
+    facebook: 'Rocketseat',
+    instagram: 'lemosgustavo_',
+    twitter: 'PrazerGusLemos'
+  }
+  
+  function changeSocialMediaLiks() {
+    for (let li of socialLinks.children) {
+      const social = li.getAttribute('class')
+      li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+  }
+  
+  changeSocialMediaLiks()
+  
+  function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userGit.href = data.html_url
+        userImage.src = data.avatar_url
+        userLogin.textContent = data.login
+      })
+  }
+  
+  getGitHubProfileInfos()
+  
